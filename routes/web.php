db3ceb0450d590032a91b0b305dbf5c25e4e81c8
MicoFeb <sutanmico@gmail.com>
2023-08-08 17:39:33 +0700

    add ✨ struktur folder

--- routes/web.php
+++ routes/web.php
@@ -43,10 +43,58 @@ Route::get('/leads/detail', function () {
 });
 
 Route::get('/leads/activity/create', function () {
-    return view('admin.leads.create');
+    return view('admin.leads.activity');
 });
 
 Route::get('/leads/offer/create', function () {
     return view('admin.leads.offer');
+});
+
+Route::get('/client/menu', function () {
+    return view('admin.client.menu');
+});
+
+Route::get('/client/detail', function () {
+    return view('admin.client.detail');
+});
+
+Route::get('/client/history', function () {
+    return view('admin.client.order.history');
+});
+
+Route::get('/client/history/detail', function () {
+    return view('admin.client.order.detail');
+});
+
+Route::get('/client/history/detail/timeline', function () {
+    return view('admin.client.order.timeline');
+});
+
+Route::get('/client/order', function () {
+    return view('admin.client.order.list');
+});
+
+Route::get('/client/create', function () {
+    return view('admin.client.order.create');
+});
+
+Route::get('/client/plan/create/recruitment', function () {
+    return view('admin.client.plan.recruitment');
+});
+
+Route::get('/client/plan/create/training', function () {
+    return view('admin.client.plan.training');
+});
+
+Route::get('/client/plan/create/penawaran', function () {
+    return view('admin.client.plan.penawaran');
+});
+
+Route::get('/client/plan/create/interview', function () {
+    return view('admin.client.plan.interview');
+});
+
+Route::get('/client/plan/create/popks', function () {
+    return view('admin.client.plan.popks');
 });
 // Admin End
