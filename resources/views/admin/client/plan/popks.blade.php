6824c8bd9ef3bacce5706f77bb19ce2adc41f910
kemalzaki <kemalmuhammadzaki@gmail.com>
2023-08-09 15:24:18 +0700

    ongoing popks

--- resources/views/admin/client/plan/popks.blade.php
+++ resources/views/admin/client/plan/popks.blade.php
@@ -2,9 +2,48 @@
 
 @section('container')
     <div class="bg-grey flex flex-col text-lightGrey p-8 rounded-md space-y-2">
-        <div class="flex flex-row justify-between">
-            <div class="text-2xl">Detail Order</div>
-            <Button class="bg-secondary rounded-md py-2 px-4 text-sm">Lihat Timeline</Button>
+        <div class="flex flex-col justify-between">
+            JagoIT:
+            <div class="flex flex-row space-x-2">
+                <div class="flex flex-col">
+                    Nama
+                    <input type="text" name="" id="">
+                </div>
+                <div class="flex flex-col ">
+                    Jabatan
+                    <input type="text" name="" id="">
+                </div>
+                <div class="flex flex-col">
+                    Alamat
+                    <input type="text" name="" id="">
+                </div>
+            </div>
+
+            Client:
+            <div class="flex flex-row space-x-2">
+                <div class="flex flex-col">
+                    Nama
+                    <input type="text" name="" id="">
+                </div>
+                <div class="flex flex-col">
+                    Jabatan
+                    <input type="text" name="" id="">
+                </div>
+                <div class="flex flex-col">
+                    Alamat
+                    <input type="text" name="" id="">
+                </div>
+            </div>
+
+            Jangka Waktu Kontrak:
+            <div class="flex flex-row space-x-2">
+                <div>Dari</div>
+                <input type="text" name="" id="">
+                <div>Sampai</div>
+                <input type="text" name="" id="">
+                </div>
+            </div>
+            
         </div>
         <div class="divide-y divide-slate-50">
             <div class="pt-3">Nama Perusahaan</div>
