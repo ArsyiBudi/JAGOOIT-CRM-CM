08c93ce048e1957d893c521069641dad5de5b07f
kemalzaki <kemalmuhammadzaki@gmail.com>
2023-08-09 12:11:38 +0700

    detail leads update

--- resources/views/admin/leads/detail.blade.php
+++ resources/views/admin/leads/detail.blade.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Detail Leads</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('admin.layouts.main')
+
+@section('container')
+<title>Detail Leads</title>
+
+<div class="bg-primary flex flex-col text-lightGrey p-8 rounded-md space-y-2">
+    <div class="text-2xl">Detail</div>
+    <div class="flex flex-row text-xs space-x-2">
+        <Button class="bg-secondary rounded-md p-1">Create Activity</Button>
+        <Button class="bg-secondary rounded-md p-1">Create Offer</Button>
+    </div>
+    <div class="divide-y divide-slate-50">
+        <div class="pt-3">Nama Perusahaan</div>
+        <div class="pt-3">Alamat</div>
+        <div class="pt-3">Nama PIC</div>
+        <div class="pt-3">No Telp PIC</div>
+        <div class="pt-3">Last Activity</div>
+        <div class="pt-3">Status</div>
+        <div class="pt-3">Keterangan</div>
+        <hr>
+    </div>
+</div>
+@endsection
