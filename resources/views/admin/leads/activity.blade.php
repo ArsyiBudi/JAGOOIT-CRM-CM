d44bb84510f28b434efc671d03c917acc51eed42
SMKN 4 BANDUNG <SMKN 4 Bandung@DESKTOP-5K32KFS>
2023-08-11 09:39:10 +0700

    pesan

--- resources/views/admin/leads/activity.blade.php
+++ resources/views/admin/leads/activity.blade.php
@@ -8,9 +8,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Create Activity</title>
     <style>
-    ::placeholder {
-        color: #999; 
-    }
+    input::placeholder,
+    textarea::placeholder {
+        color: black;
+        font-family: quicksand;
+        font-size: 15px;
+}
     textarea {
         display: block;
         width: 100%;
@@ -23,50 +26,39 @@
     </style>
 </head>
 <body>
-<<<<<<< HEAD
 
 <div class="bg-darkSecondary p-6 rounded-lg shadow-lg mx-auto w-[900px] h-[500px]"> 
         <h6 class="text-2xl font-semibold mb-4 ml-2">Create Activity</h6>
-        <select id="formSelector" class="mb-4 ml-2 bg-primary border border-none rounded-md py-1">Appointment
-=======
-<h6 class="text-2xl font-semibold mb-4 ml-36">Create Activity</h6>
-<div class="bg-gray-500 p-6 rounded-lg shadow-lg mx-auto w-[900px] h-[500px]"> 
-        
-        <select id="formSelector" class="mb-4 bg-gray-500 border border-none rounded-md py-1">Appointment
->>>>>>> 70542059cf8a4f3f1de638eca4b748afad03c809
-            <option value="form1">Appointment</option>
-            <option value="form2">Note</option>
-            <option value="form3">Report</option>
+        <select id="formSelector" class="mb-4 ml-2 bg-primary font-quicksand border border-none rounded-md py-1">Appointment
+            <option value="form1" class='font-quicksand'>Appointment</option>  
+            <option value="form2" class='font-quicksand'>Note</option>
+            <option value="form3" class='font-quicksand'>Report</option>
         </select>
         <div class="flex">
             <div id="formContainer" class="w-full">
                 <form id="form1" class="hidden">
-<<<<<<< HEAD
-                    <textarea name="judul" id="judul" class="bg-whitey mb-2 p-2 border rounded w-full" placeholder="judul"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
-=======
-                    <textarea name="judul" id="judul" class="bg-gray-300 mb-2 p-2 border rounded w-full" placeholder="Judul"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
->>>>>>> 70542059cf8a4f3f1de638eca4b748afad03c809
+                    <textarea name="judul" id="judul" class="bg-white font-quicksand text-black mb-2 p-2 border rounded w-full" placeholder="judul"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
                     <div class="flex mb-5">
-                        <textarea name="lokasi" id="lokasi" class="bg-gray-300 p-2 border rounded mr-2 flex-1" placeholder="lokasi"></textarea>
-                        <textarea name="waktu" id="waktu" class="bg-gray-300 p-2 border rounded flex-1" placeholder="waktu"></textarea>
+                        <textarea name="lokasi" id="lokasi" class="bg-white text-black font-quicksand p-2 border rounded mr-2 flex-1" placeholder="lokasi"></textarea>
+                        <textarea name="waktu" id="waktu" class="bg-white text-black p-2 font-quicksand border rounded flex-1" placeholder="waktu"></textarea>
                     </div>
-                    <textarea name="deskripsi" id="deskripsi" class="bg-gray-300 mb-2 p-2 border rounded w-full" placeholder="deskripsi"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
+                    <textarea name="deskripsi" id="deskripsi" class="bg-white font-quicksand text-black mb-2 p-2 border rounded w-full" placeholder="deskripsi"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
                     <div class="w-[97px] mx-auto">
-                        <input type="submit" class="bg-secondary text-white rounded-md px-4 py-2 h-[37px] mt-11">
+                        <input type="submit" class="bg-secondary text-white font-quicksand rounded-md px-4 py-2 h-[37px] mt-11">
                     </div>
                 </form>
                 <form id="form2" class="hidden">
-                    <textarea name="deskripsinote" id="deskripsinote" class="bg-gray-300 mb-2 p-2 border rounded w-[820px] h-[270px]" placeholder="deskripsi"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
+                    <textarea name="deskripsinote" id="deskripsinote" class="bg-white text-black font-quicksand mb-2 p-2 border rounded w-[820px] h-[270px]" placeholder="deskripsi"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
                     <div class="w-[97px] mx-auto">
                         <input type="submit" class="bg-secondary text-white rounded-md px-4 mt-5 py-2 h-[37px]">
                     </div>
                 </form>
                 <form id="form3" class="hidden">
-                    <textarea name="judulreport" id="judulreport" class="bg-gray-300 mb-2 p-2 border rounded w-full" placeholder="judul"></textarea>
-                    <input type="file"name="file" id="file" class="bg-gray-300 mb-3 mt-5 p-2 ml-2 border rounded w-full" placeholder="file"></input>
-                    <textarea name="deskripsireport" id="deskripsireport" class="bg-gray-300 mt-5 mb-2 p-2 border rounded w-full h-[100px]" placeholder="deskripsi"></textarea> 
+                    <textarea name="judulreport" id="judulreport" class="bg-white text-black font-quicksand mb-2 p-2 border rounded w-full" placeholder="judul"></textarea>
+                    <input type="file"name="file" id="file" class="bg-white text-black mb-3 mt-5 p-2 ml-2 border rounded w-full" placeholder="file"></input>
+                    <textarea name="deskripsireport" id="deskripsireport" class="bg-white text-black font-quicksand mt-5 mb-2 p-2 border rounded w-full h-[100px]" placeholder="deskripsi"></textarea> 
                     <div class="w-[97px] mx-auto">
-                        <input type="submit" class="bg-secondary text-white rounded-md mt-4 mr-3 px-4 py-2 h-[37px]">
+                        <input type="submit" class="bg-secondary rounded-md mt-4 mr-3 px-4 py-2 h-[37px]">
                     </div>  
                 </form>
             </div>
