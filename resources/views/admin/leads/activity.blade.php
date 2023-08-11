db213f0bdd565558773c9cbc520fc2df0a7ce428
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2023-08-11 10:53:25 +0700

    create activity fixed and create offer still ongoing

--- resources/views/admin/leads/activity.blade.php
+++ resources/views/admin/leads/activity.blade.php
@@ -1,66 +1,60 @@
 @extends('admin.layouts.main')
+
 @section('container')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Activity</title>
-    <style>
-    ::placeholder {
-        color: #999; 
-    }
-    textarea {
-        display: block;
-        width: 100%;
-        padding: 8px;
-        margin: 10px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        resize: vertical; /* Agar dapat diresize secara vertikal */
-    }
-    </style>
-</head>
-<body>
-<h6 class="text-2xl font-semibold mb-4 ml-36">Create Activity</h6>
-<div class="bg-gray-500 p-6 rounded-lg shadow-lg mx-auto w-[900px] h-[500px]"> 
+<h6 class=" font-normal mb-4 text-4xl">Create Activity</h6>
+<div class="bg-grey p-9 rounded-lg border-2 border-white  w-full h-[500px]"> 
         
-        <select id="formSelector" class="mb-4 bg-gray-500 border border-none rounded-md py-1">Appointment
-            <option value="form1">Appointment</option>
-            <option value="form2">Note</option>
-            <option value="form3">Report</option>
+        <select id="formSelector" class="mb-4 bg-transparent border outline-none border-none rounded-md py-1 text-3xl">
+            <option value="form1" class="bg-grey">Appointment</option>
+            <option value="form2" class="bg-grey">Note</option>
+            <option value="form3" class="bg-grey">Report</option>
         </select>
-        <div class="flex">
             <div id="formContainer" class="w-full">
                 <form id="form1" class="hidden">
-                    <textarea name="judul" id="judul" class="bg-gray-300 mb-2 p-2 border rounded w-full" placeholder="Judul"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
-                    <div class="flex mb-5">
-                        <textarea name="lokasi" id="lokasi" class="bg-gray-300 p-2 border rounded mr-2 flex-1" placeholder="lokasi"></textarea>
-                        <textarea name="waktu" id="waktu" class="bg-gray-300 p-2 border rounded flex-1" placeholder="waktu"></textarea>
+                    <div class="bg-white opacity-70 mb-4 p-2 rounded-md w-full">
+                        <textarea name="judul" id="judul" class="text-black opacity-100 w-full p-2 bg-transparent outline-none resize-none" placeholder="Judul"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
                     </div>
-                    <textarea name="deskripsi" id="deskripsi" class="bg-gray-300 mb-2 p-2 border rounded w-full" placeholder="deskripsi"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
+                    <div class="flex gap-2 mb-4 w-full">
+                        <div class="bg-white opacity-70 rounded-md mr-2 w-1/2 p-2">
+                            <textarea name="lokasi" id="lokasi" class="bg-transparent text-black p-2 w-full outline-none resize-none" placeholder="Lokasi"></textarea>
+                        </div>
+                        <div class="bg-white opacity-70 rounded-md  w-1/2 p-2">
+                            <textarea name="waktu" id="waktu" class="bg-transparent text-black p-2 w-full outline-none resize-none" placeholder="Waktu"></textarea>
+                        </div>
+                        
+                    </div>
+                    <div class="bg-white opacity-70 rounded-md w-full p-2 h-[100px]">
+                        <textarea name="deskripsi" id="deskripsi" class="bg-transparent outline-none p-2 text-black resize-none h-full w-full" placeholder="Deskripsi"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
+                    </div>
                     <div class="w-[97px] mx-auto">
-                        <input type="submit" class="bg-secondary text-white rounded-md px-4 py-2 h-[37px] mt-11">
+                        <input type="submit" class="bg-secondary  text-white rounded-md px-4 py-2 h-[37px] mt-11">
                     </div>
                 </form>
                 <form id="form2" class="hidden">
-                    <textarea name="deskripsinote" id="deskripsinote" class="bg-gray-300 mb-2 p-2 border rounded w-[820px] h-[270px]" placeholder="deskripsi"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
+                    <div class="bg-white opacity-70 rounded-md w-full mb-2 p-2 h-[270px]">
+                        <textarea name="deskripsinote" id="deskripsinote" class="bg-transparent p-2 outline-none text-black w-full h-full resize-none" placeholder="Deskripsi"></textarea> <!-- Menggunakan w-full untuk mengisi textarea secara penuh -->
+                    </div>
                     <div class="w-[97px] mx-auto">
                         <input type="submit" class="bg-secondary text-white rounded-md px-4 mt-5 py-2 h-[37px]">
                     </div>
                 </form>
                 <form id="form3" class="hidden">
-                    <textarea name="judulreport" id="judulreport" class="bg-gray-300 mb-2 p-2 border rounded w-full" placeholder="judul"></textarea>
-                    <input type="file"name="file" id="file" class="bg-gray-300 mb-3 mt-5 p-2 ml-2 border rounded w-full" placeholder="file"></input>
-                    <textarea name="deskripsireport" id="deskripsireport" class="bg-gray-300 mt-5 mb-2 p-2 border rounded w-full h-[100px]" placeholder="deskripsi"></textarea> 
+                    <div class="bg-white opacity-70 rounded-md w-full mb-4 p-2">
+                        <textarea name="judulreport" id="judulreport" class="bg-transparent outline-none w-full p-2 resize-none text-black" placeholder="Judul"></textarea>
+                    </div>
+                    <div class="bg-white opacity-70 rounded-md w-full mb-4 p-4">
+                        <input type="file" name="file" id="file" class="hidden w-full" placeholder="file"></input>
+                        <label for="file" class="text-grey w-full block">File</label>
+                    </div>
+                    <div class="bg-white opacity-70 rounded-md w-full mb-4 p-2 h-[100px]">
+                        <textarea name="deskripsireport" id="deskripsireport" class="bg-transparent outline-none p-2  rounded w-full h-full resize-none text-black" placeholder="Deskripsi"></textarea> 
+                    </div>
                     <div class="w-[97px] mx-auto">
-                        <input type="submit" class="bg-secondary text-white rounded-md mt-4 mr-3 px-4 py-2 h-[37px]">
+                        <input type="submit" class="bg-secondary text-white rounded-md mt-5 mr-3 px-4 py-2 h-[37px]">
                     </div>
                 </form>
             </div>
-        </div>
-    </div>
+</div>
     <script>
         document.addEventListener("DOMContentLoaded", function () {
             const formSelector = document.getElementById("formSelector");
@@ -84,6 +78,4 @@
             document.getElementById("form1").style.display = "block";
         });
     </script>
-</body>
-</html>
 @endsection
--- resources/views/admin/leads/create.blade.php
+++ resources/views/admin/leads/create.blade.php
@@ -37,7 +37,7 @@
 
 
 @section('container')
-    <div class=" hide-scrollbar w-full rounded shadow-lg bg-primary h-auto md:h-[580px]  pb-20 overflow-hidden mt-48 md:mt-0">
+    <form class=" hide-scrollbar w-full rounded shadow-lg bg-primary h-auto md:h-[580px]  pb-20 overflow-hidden mt-48 md:mt-0">
         <div class="px-6 py-4 overflow-auto">
             <div class="mb-2 text-3xl pb-5 border-white border-b mt-12"> 
                 <h1>Create Leads</h1> 
@@ -47,35 +47,35 @@
                 <div class="kiri flex flex-col items-start">
                     <div class="mt-8">
                         <h4>Nama Perusahaan</h4>
-                        <input type="text" class="text-black rounded-lg mt-1 w-full md:w-96 py-3 px-3 outline-none">
+                        <input type="text" class="text-black bg-white rounded-lg mt-1 w-full md:w-96 py-3 px-3 outline-none">
                     </div>
                     
                     <div class="mt-8">
                         <h4 class="mb-1">Alamat Perusahaan</h4>
-                        <input type="text" class="text-black rounded-lg w-full md:w-96 py-6 px-3 outline-none">
+                        <input type="text" class="text-black bg-white rounded-lg w-full md:w-96 py-6 px-3 outline-none">
                     </div>
 
                     <div class="mt-8">
                         <h4 class="mb-1">Nama PIC</h4>
-                        <input type="text" class="text-black rounded-lg w-full md:w-96 py-3 px-3 outline-none">
+                        <input type="text" class="text-black bg-white rounded-lg w-full md:w-96 py-3 px-3 outline-none">
                     </div>
                 </div>
             
                 <div class="flex flex-col items-end ml-10">
                     <div class="mt-8">
                         <h4>Email</h4>
-                        <input type="text" class="text-black rounded-lg mt-1 w-full md:w-96 py-3 px-3 outline-none">
+                        <input type="text" class="text-black bg-white rounded-lg mt-1 w-full md:w-96 py-3 px-3 outline-none">
                     </div>
                     
                     <div class="mt-8">
                         <h4 class="mb-1">Bidang</h4>
-                        <input type="text" class="text-black rounded-lg w-full md:w-96 py-6 px-3 outline-none">
+                        <input type="text" class="text-black bg-white rounded-lg w-full md:w-96 py-6 px-3 outline-none">
 
                     </div>
 
                     <div class="mt-8">
                         <h4 class="mb-1">Nomor Kontakk PIC</h4>
-                        <input type="text" class="text-black rounded-lg mt-1 w-full md:w-96 py-3 px-3 outline-none">
+                        <input type="text" class="text-black bg-white rounded-lg mt-1 w-full md:w-96 py-3 px-3 outline-none">
 
                     </div>
                 </div>
@@ -83,7 +83,7 @@
 
             <div class="mt-8 flex justify-between pr-5"> <!-- Menggunakan kelas justify-between untuk mengatur posisi tombol -->
                 <button class="bg-secondary text-white rounded-md px-4 py-2 h-[37px] w-[97px]">Back</button>
-                <button class="bg-secondary text-white rounded-md px-4 py-2 h-[37px] w-[198px]">Create</button>
+                <input type="submit"  value="Create" class="bg-secondary text-white rounded-md px-4 py-2 h-[37px] w-[198px]"></input>
             </div>
     </div>
 @endsection
