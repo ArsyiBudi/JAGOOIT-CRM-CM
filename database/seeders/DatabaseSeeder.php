c0111cf3bac7d79dc2cda81d355a47f29399b487
Harmoni Natanael S <harmoninatanaels@gmail.com>
2023-08-15 06:35:29 +0700

    clearing test file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Talent::factory(10)->create();
+        // Talent::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
