6552e60a02cc9a704f696e7a447aeb9b5646307e
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2023-08-15 08:33:03 +0700

    leads menu all responsive, detail leads all responsive, create activity leads all responsive

--- resources/views/admin/leads/detail.blade.php
+++ resources/views/admin/leads/detail.blade.php
@@ -1,13 +1,13 @@
 @extends('admin.layouts.main')
 
 @section('container')
-    <div class="bg-primary flex flex-col text-lightGrey p-8 rounded-md space-y-2 w-full">
+    <div class="bg-primary flex flex-col text-lightGrey p-8 rounded-md space-y-2 w-full mt-20">
         <div class="text-2xl">Detail Leads</div>
         <div class="flex flex-row text-xs space-x-2">
             <a class="bg-secondary rounded-md px-4 py-2">Create Activity</a>
             <a class="bg-secondary rounded-md px-4 py-2">Create Offer</a>
         </div>
-        <div class="divide-y divide-slate-50">
+        <div class="divide-y divide-slate-50 gap-4 flex flex-col">
             <div class="pt-3">
                 <p>
                     Nama Perusahaan
