bb65cd88e005f1b0d016097d676f1456d4ed78a1
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2023-08-15 13:10:58 +0700

    menu leads small changes, menu client small changes

--- resources/views/admin/client/menu.blade.php
+++ resources/views/admin/client/menu.blade.php
@@ -36,33 +36,29 @@
 </style>
 
 @section('container')
-  <div class=" overflow-auto pt-28 md:pt-0 h-screen">
-    <div class="w-full bg-darkSecondary pt-5 pb-16 px-7 flex  flex-col rounded-md">
+  <div class=" overflow-auto mt-28 lg:mt-0">
+    <div class="w-full bg-darkSecondary py-10 px-8 flex  flex-col rounded-md">
 
         <div class="border-b border-white w-full pb-3 mb-3">
             <h3 class="text-white font-semibold text-3xl">Data Clients</h3>
         </div>
 
-        <a href="/client/create">
-            <button class="bg-secondary hover:scale-95 duration-200 md:w-[143px] w-full rounded-lg py-2 flex items-center justify-center">
-                Create Orders 
-            </button>
-        </a>
-
-        <form class=" block md:flex items-center justify-between mt-5 md:mt-3 mb-10">
-            <div class="flex gap-3 items-center justify-center md:justify-normal">
-                <p class="text-white text-sm">Show</p>
-                <div class="flex items-center bg-grey rounded-xl justify-center py-1 w-[60px] px-1">
-                    <input type="text" class=" text-white w-full text-center bg-transparent outline-none" placeholder="5">
+        <form class=" block md:flex items-start my-4 justify-between mb-8 w-full">
+            <div class=" md:block flex items-center justify-between">
+                <div class="flex gap-3 items-center justify-start">
+                    <p class="text-white text-xs md:text-sm">Show</p>
+                    <div class="flex items-center bg-grey rounded-md md:rounded-lg justify-center py-0 md:py-1 w-[40px] md:w-[60px] px-1">
+                        <input type="text" class=" text-white w-full text-center bg-transparent outline-none" placeholder="5">
+                    </div>
+                    <p class="text-white text-xs md:text-sm">entries</p>
                 </div>
-                <p class="text-white text-sm">entries</p>
-
+                <div class="md:mt-5">
+                        <a class=" bg-secondary font-semibold text-sm md:text-[16px] py-1 px-3 md:px-5 rounded-lg mt-0 md:mt-7 hover:scale-95 duration-200" href="/client/create">Create Orders</a>  
+                </div>
             </div>
-            <div class=" block md:flex gap-3 items-center mt-2 md:mt-0">
-                <p class="text-white text-sm">Search</p>
-                <div class="bg-white rounded-xl py-1 px-1 w-[320px]  mt-2 md:mt-0">
-                    <input type="text" class="text-black bg-transparent outline-none w-full px-2">
-                </div>
+            <div class=" flex items-center gap-5 mt-5 md:mt-0">
+                <p class=" hidden md:block">Search</p>
+                <input type="text" class=" outline-none bg-white rounded-md w-full md:w-80 py-1 px-2 text-black font-semibold placeholder-gray-400 placeholder-opacity-100 md:placeholder-opacity-0" placeholder="search">
             </div>
         </form>
 
