9c366d89dcdb7bb6e44809996ac518398a3dc9c3
MicoFeb <sutanmico@gmail.com>
2023-08-23 10:47:14 +0700

    add ✨ dynamic input create leads

--- resources/views/admin/leads/create.blade.php
+++ resources/views/admin/leads/create.blade.php
@@ -37,8 +37,8 @@
 
 
 @section('container')
-    <div class=" overflow-auto pt-28 lg:pt-0 h-screen">
-        <form class=" w-full bg-darkSecondary py-10 px-8 rounded-md" action="{{ route('create_order') }}" method="post">
+    <div class="hide-scrollbar overflow-auto pt-28 lg:pt-0 h-screen w-full">
+        <form class="  w-full bg-darkSecondary py-10 px-8 rounded-md" action="{{ route('create_order') }}" method="post">
             @csrf
             <div class="px-0 md:px-5 py-0 md:py-4 overflow-auto">
                 <div class="mb-2 text-3xl pb-5 border-white border-b "> 
@@ -49,16 +49,14 @@
                     <div class=" block md:flex w-full gap-14 mt-2 md:mt-0" >
                         <div class="">
                             <h4 class=" text-sm md:text-[16px]">Nama Perusahaan</h4>
-                            <input type="text" name="business_name" class="text-black bg-white rounded-lg mt-1 w-full md:w-96  py-3 px-3 outline-none">
+                            <input required type="text" placeholder="Nama Perusahaan" name="business_name" class="text-black bg-white rounded-lg mt-2 w-full md:w-96  py-3 px-3 outline-none">
                         </div>
 
                         <div class=" mt-3 md:mt-0">
                             <h4  class=" text-sm md:text-[16px]">Email</h4>
-                            <div class=" flex items-center gap-5">
-                                <div id="inputContainer">
-                                    <input type="text" class="bg-white text-black rounded-lg mt-1 w-full md:w-80 lg:w-96  py-2 px-3 outline-none">
-                                </div>
-                                <div class=" bg-secondary py-2 px-4 rounded-md hover:scale-95 duration-200 cursor-pointer" id="addInputBtn">
+                            <div class=" flex items-start  gap-5">
+                                <div id="inputContainer"></div>
+                                <div class=" bg-secondary py-2 px-4 rounded-md hover:scale-95 duration-200 cursor-pointer mt-2" id="addInputBtn">
                                     <i class="ri-add-line"></i>
                                 </div>
                             </div>
@@ -68,24 +66,24 @@
                     <div class=" block md:flex w-full  gap-14">
                         <div class="">
                             <h4 class="mb-1 text-sm md:text-[16px]">Alamat Perusahaan</h4>
-                            <input type="text" name="address" class="text-black bg-white rounded-lg w-full md:w-96  py-6 px-3 outline-none">
+                            <input required placeholder="Alamat Perusahaan" type="text" name="address" class="text-black bg-white rounded-lg w-full md:w-96  py-6 px-3 outline-none">
                         </div>
 
                         <div class=" mt-3 md:mt-0">
                             <h4 class="mb-1 text-sm md:text-[16px]">Bidang</h4>
-                            <input type="text" name="business_sector" class="bg-white text-black rounded-lg w-full md:w-80 lg:w-96  py-6 px-3 outline-none">
+                            <input required placeholder="Bidang" type="text" name="business_sector" class="bg-white text-black rounded-lg w-full md:w-80 lg:w-96  py-6 px-3 outline-none">
                         </div>
                     </div>
 
                       <div class=" block md:flex w-full  gap-14">
                         <div class="">
                             <h4 class="mb-1 text-sm md:text-[16px]">Nama PIC</h4>
-                            <input type="text" name="pic_name" class="bg-white text-black rounded-lg w-full md:w-96  py-3 px-3 outline-none">
+                            <input required placeholder="Nama PIC" type="text" name="pic_name" class="bg-white text-black rounded-lg w-full md:w-96  py-3 px-3 outline-none">
                         </div>
 
                         <div class=" mt-3 md:mt-0">
                             <h4 class="mb-1 text-sm md:text-[16px]">Nomor Kontak PIC</h4>
-                            <input type="text" name="pic_contact_number" class="bg-white text-black rounded-lg mt-1 w-full md:w-80 lg:w-96  py-3 px-3 outline-none">
+                            <input required placeholder="089xxxxxxxxx" type="text" name="pic_contact_number" class="bg-white text-black rounded-lg mt-1 w-full md:w-80 lg:w-96  py-3 px-3 outline-none">
                         </div>
                     </div>
                 </div>
@@ -115,9 +113,9 @@
                 const inputField = document.createElement('input');
                 inputField.type = 'text';
                 inputField.name = `input_${data.id}`;
-                inputField.placeholder = `Input ${data.id}`;
+                inputField.placeholder = `Email ${data.id}`;
                 inputField.id = `${data.id}`
-                inputField.className = 'text-black bg-white rounded-lg mt-1 w-full md:w-96 py-3 px-3 outline-none';
+                inputField.className = 'text-black bg-white rounded-lg mt-2 w-full md:w-96 py-3 px-3 outline-none';
                 
                 inputContainer.appendChild(inputField);
             });
