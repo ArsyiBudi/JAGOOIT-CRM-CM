15e9782e3bba162a32cccce2337eb9b41048b12b
kemalzaki <kemalmuhammadzaki@gmail.com>
2023-08-23 09:35:00 +0700

    fix structure form and minor fiks popks

--- resources/views/admin/client/plan/popks.blade.php
+++ resources/views/admin/client/plan/popks.blade.php
@@ -32,191 +32,190 @@
             </ul>
         </div>
 
-        <div class="pl-4 mt-5">
-            <form action="">
-                <div class=" block md:flex justify-end">
-                    <div class="flex gap-3 items-center w-full md:w-auto">
-                        <label for="endDate">End Date: </label>
-                        <input type="date" id="endDate" class=" custom-date-input rounded-md bg-primary py-2 px-5 text-white outline-none border-[1px] border-white">
-                    </div>
-                </div>
-            </form>
-        </div>
 
-
-        <div class="overflow-auto bg-darkSecondary mt-5 justify-between flex flex-col text-white px-8 py-10 rounded-md gap-y-3">
         <form action="{{ route('save_popks') }}" method="post">
-            
             @csrf
-            <div class="text-xl">JagoIT:</div>
-            <div class="flex flex-row flex-wrap gap-2">
-                <div class="flex-auto flex flex-col">
-                    Nama
-                    <input class="rounded-md text-black" type="text" name="" id="">
-                </div>
-                <div class="flex-auto flex flex-col ">
-                    Jabatan
-                    <input class="rounded-md text-black" type="text" name="" id="">
-                </div>
-                <div class="flex-auto flex flex-col">
-                    Alamat
-                    <input class="rounded-md text-black" type="text" name="" id="">
-                </div>
-            </div>
-    
-            <div class="text-xl mt-7">Client:</div>
-            <div class="flex flex-row flex-wrap gap-2">
-                <div class="flex-auto flex flex-col">
-                    Nama
-                    <input class="rounded-md text-black" type="text" name="" id="">
-                </div>
-                <div class="flex-auto flex flex-col">
-                    Jabatan
-                    <input class="rounded-md text-black" type="text" name="" id="">
-                </div>
-                <div class="flex-auto flex flex-col text-black">
-                    Alamat
-                    <input class="rounded-md" type="text" name="" id="">
-                </div>
-            </div>
-    
-            <div class="text-xl mt-7">Jangka Waktu Kontrak:</div>
-            <div class="flex flex-row flex-wrap gap-2">
-                <div>Dari</div>
-                <input class="rounded-md flex-auto text-black" type="text" name="" id="">
-                <div>Sampai</div>
-                <input class="rounded-md flex-auto text-black" type="text" name="" id="">
-            </div>
-    
-            <div class="text-xl mt-7">Biaya Kontrak</div>
-            <div class="flex flex-row flex-wrap gap-2">
-                <div class="flex-auto flex flex-col">
-                    Termasuk Biaya
-                    <input type="text" class="rounded-md text-black">
-                </div>
-                <div class="flex-auto flex flex-col">
-                    Nominal
-                    <input type="text" class="rounded-md text-black">
+            <div class="pl-4 mt-5 block md:flex justify-end">
+                <div class="flex gap-3 items-center w-full md:w-auto">
+                    <label for="endDate">End Date: </label>
+                    <input type="date" id="endDate" class=" custom-date-input rounded-md bg-primary py-2 px-5 text-white outline-none border-[1px] border-white">
                 </div>
             </div>
 
-            <div class="text-xl mt-7">Biaya Overtime (Perjam)</div>
-            <div class="flex flex-row flex-wrap gap-2">
-                <div class="flex-auto flex flex-col">
-                    <div>Weekday</div>
-                    <input type="text" class="rounded-md text-black">
-                    <div>Weekend</div>
-                    <input type="text" class="rounded-md text-black">
-                </div>
-                <div class="flex-auto flex flex-col">
-                    <label for="catatan-popks">Catatan</label>
-                    <textarea id="catatan-popks" type="text" class=" text-black rounded-md h-full hide-scrollbar resize-none"></textarea>
-                </div>
-            </div>
+            <div class="overflow-auto bg-darkSecondary mt-5 justify-between flex flex-col text-white px-8 py-10 rounded-md gap-y-3">
 
-            <div class="text-xl mt-7">Biaya Dinas</div>
-            <div class="flex flex-row flex-wrap gap-2">
-                <div class="flex-auto flex flex-col">
-                    <div>Konsumsi (perhari)</div>
-                    <input type="text" class="rounded-md text-black">
-                </div>
-                <div class="flex-auto flex flex-col">
-                    Transport Pulang-Pergi Standar JKT-BDG
-                    <input type="text" class="rounded-md h-full text-black">
-                </div>
-            </div>
-
-            <div class="text-xl mt-7">Invoice Tagihan</div>
-            <div class="flex flex-row flex-wrap gap-2">
-                Tanggal 
-                <input type="date" class="w-10 text-black rounded-md">
-                Setiap Bulan dan Pembayaran Selambat-Lambatnya
-                <input type="number" class="text-black rounded-md">
-                Hari
-            </div>
-
-            <div class="text-xl mt-7">Rekening JagooIT</div>
-            <div class="flex flex-row flex-wrap gap-2">
-                <div class="flex-auto flex flex-col">
-                    Atas Nama
-                    <input type="text" class="rounded-md text-black">
-                </div>
-                <div class="flex-auto flex flex-col">
-                    Nama Bank
-                    <input type="text" class="rounded-md text-black">
-                </div>
-            </div>
-            <div class="flex-auto flex flex-col">
-                No. Rekening
-                <input type="text" class="rounded-md text-black">
-            </div>
-
-            <div class="text-xl mt-7">Mengetahui</div>
-            <div class="flex flex-row flex-wrap gap-2">
-                <div class="flex-auto flex flex-col">
-                    Direktur JagooIT
-                    <input type="text" class="rounded-md text-black">
-                </div>
-                <div class="flex-auto flex flex-col">
-                    Direktur Klien
-                    <input type="text" class="rounded-md text-black">
-                </div>
-            </div>
-            
-            <div class="mt-4 flex justify-end">
-                <button type="submit" name="createWord" class=" w-full  md:w-[188px] bg-secondary text-white text-sm text-center h-[37px] rounded-md hover:scale-95 duration-200">Create</button>
-            </div>
-
-        </div>
-    </form>
-
-        <div class="overflow-auto bg-darkSecondary mt-5 justify-between flex flex-col text-lightGrey px-8 py-10 rounded-md space-y-3">
-            <div>File PKS (1 file, pdf)</div>
-            <div class="flex flex-row space-x-2">
-                <div class="flex-auto flex flex-col">
-                    <div class="flex justify-center items-center bg-white py-2 rounded-lg h-[56px] w-[335px]">
-                        <input id="file-cv" type="file" class="text-black rounded-lg hidden bg-white" name="">
-                        <label for="file-cv" class="cursor-pointer">
-                            <i class="ri-upload-2-fill text-3xl text-black"></i>
-                        </label>
+                <div class="text-xl">JagoIT:</div>
+                <div class="flex flex-row flex-wrap gap-2">
+                    <div class="flex-auto flex flex-col">
+                        Nama
+                        <input class="rounded-md text-black" type="text" name="" id="">
+                    </div>
+                    <div class="flex-auto flex flex-col ">
+                        Jabatan
+                        <input class="rounded-md text-black" type="text" name="" id="">
+                    </div>
+                    <div class="flex-auto flex flex-col">
+                        Alamat
+                        <input class="rounded-md text-black" type="text" name="" id="">
                     </div>
                 </div>
-                <div class="">
-                    <input type="checkbox" class="rounded-md"> File PO Diterima
-                </div>    
-            </div>
-
-            <div class="flex-auto flex flex-col">
-                <label for="desk-popks">Deskripsi</label>
-                <textarea id="desk-popks" type="text" class="rounded-md h-[70px] text-black hide-scrollbar resize-none"></textarea>
-            </div>
-
-            <div class="mt-4 flex justify-end">
-                <button type="submit" name="Send" class=" w-full  md:w-[188px] bg-secondary text-white text-sm text-center h-[37px] rounded-md hover:scale-95 duration-200">Send</button>
-            </div>
-        </div>
-
-        <div class=" flex justify-between items-center pt-4 md:mb-0">
-            <div>
-                <a href="/client/order/plan/percobaan" class=" bg-secondary text-white text-sm text-center py-1 px-3 md:px-14 rounded-md font-bold hover:scale-95 duration-200">
-                    <p class=" hidden md:inline">Back</p>        
-                    <i class="ri-arrow-left-line inline md:hidden"></i>  
-                </a>
-            </div>
-            
-            <div class="flex gap-4 max-sm:w-full max-sm:justify-between">
-                <div></div>
-                <div>
-                    <button type="submit" name="save" class=" w-full bg-secondary text-white text-sm text-center py-1 px-14 rounded-md font-bold hover:scale-95 duration-200">Save</button>
+        
+                <div class="text-xl mt-7">Client:</div>
+                <div class="flex flex-row flex-wrap gap-2">
+                    <div class="flex-auto flex flex-col">
+                        Nama
+                        <input class="rounded-md text-black" type="text" name="" id="">
+                    </div>
+                    <div class="flex-auto flex flex-col">
+                        Jabatan
+                        <input class="rounded-md text-black" type="text" name="" id="">
+                    </div>
+                    <div class="flex-auto flex flex-col">
+                        Alamat
+                        <input class="rounded-md text-black" type="text" name="" id="">
+                    </div>
                 </div>
-    
-                <div>
-                    <button class=" bg-secondary text-white text-sm text-center py-1 px-3 md:px-14 rounded-md font-bold hover:scale-95 duration-200">
-                      <p class=" hidden md:block">Continue</p> 
-                      <i class="ri-arrow-right-line block md:hidden"></i>
-                    </button>
+        
+                <div class="text-xl mt-7">Jangka Waktu Kontrak:</div>
+                <div class="flex flex-row flex-wrap gap-2">
+                    <div>Dari</div>
+                    <input class="rounded-md flex-auto text-black" type="text" name="" id="">
+                    <div>Sampai</div>
+                    <input class="rounded-md flex-auto text-black" type="text" name="" id="">
+                </div>
+        
+                <div class="text-xl mt-7">Biaya Kontrak</div>
+                <div class="flex flex-row flex-wrap gap-2">
+                    <div class="flex-auto flex flex-col">
+                        Termasuk Biaya
+                        <input type="text" class="rounded-md text-black">
+                    </div>
+                    <div class="flex-auto flex flex-col">
+                        Nominal
+                        <input type="text" class="rounded-md text-black">
+                    </div>
+                </div>
+
+                <div class="text-xl mt-7">Biaya Overtime (Perjam)</div>
+                <div class="flex flex-row flex-wrap gap-2">
+                    <div class="flex-auto flex flex-col">
+                        <div>Weekday</div>
+                        <input type="text" class="rounded-md text-black">
+                        <div>Weekend</div>
+                        <input type="text" class="rounded-md text-black">
+                    </div>
+                    <div class="flex-auto flex flex-col">
+                        <label for="catatan-popks">Catatan</label>
+                        <textarea id="catatan-popks" type="text" class=" text-black rounded-md h-full hide-scrollbar resize-none"></textarea>
+                    </div>
+                </div>
+
+                <div class="text-xl mt-7">Biaya Dinas</div>
+                <div class="flex flex-row flex-wrap gap-2">
+                    <div class="flex-auto flex flex-col">
+                        <div>Konsumsi (perhari)</div>
+                        <input type="text" class="rounded-md text-black">
+                    </div>
+                    <div class="flex-auto flex flex-col">
+                        Transport Pulang-Pergi Standar JKT-BDG
+                        <input type="text" class="rounded-md h-full text-black">
+                    </div>
+                </div>
+
+                <div class="text-xl mt-7">Invoice Tagihan</div>
+                <div class="flex flex-row flex-wrap gap-2">
+                    Tanggal 
+                    <input type="date" class="w-10 text-black rounded-md">
+                    Setiap Bulan dan Pembayaran Selambat-Lambatnya
+                    <input type="number" class="text-black rounded-md">
+                    Hari
+                </div>
+
+                <div class="text-xl mt-7">Rekening JagooIT</div>
+                <div class="flex flex-row flex-wrap gap-2">
+                    <div class="flex-auto flex flex-col">
+                        Atas Nama
+                        <input type="text" class="rounded-md text-black">
+                    </div>
+                    <div class="flex-auto flex flex-col">
+                        Nama Bank
+                        <input type="text" class="rounded-md text-black">
+                    </div>
+                </div>
+                <div class="flex-auto flex flex-col">
+                    No. Rekening
+                    <input type="text" class="rounded-md text-black">
+                </div>
+
+                <div class="text-xl mt-7">Mengetahui</div>
+                <div class="flex flex-row flex-wrap gap-2">
+                    <div class="flex-auto flex flex-col">
+                        Direktur JagooIT
+                        <input type="text" class="rounded-md text-black">
+                    </div>
+                    <div class="flex-auto flex flex-col">
+                        Direktur Klien
+                        <input type="text" class="rounded-md text-black">
+                    </div>
+                </div>
+                
+                <div class="mt-4 flex justify-end">
+                    <button type="submit" name="createWord" class=" w-full  md:w-[188px] bg-secondary text-white text-sm text-center h-[37px] rounded-md hover:scale-95 duration-200">Create</button>
+                </div>
+
+            </div>
+
+            <div class="overflow-auto bg-darkSecondary mt-5 justify-between flex flex-col text-lightGrey px-8 py-10 rounded-md space-y-3">
+                <div>File PKS (1 file, pdf)</div>
+                <div class="flex flex-row gap-2 flex-wrap">
+                    <div class="flex flex-col w-full sm:w-[335px]">
+                        <div class="flex justify-center items-center bg-white py-2 rounded-lg h-[56px] w-full">
+                            <input id="file-cv" type="file" class="text-black rounded-lg hidden bg-white" name="">
+                            <label for="file-cv" class="cursor-pointer">
+                                <i class="ri-upload-2-fill text-3xl text-black"></i>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="flex-auto">
+                        <input type="checkbox" class="rounded-md"> File PO Diterima
+                    </div>    
+                </div>
+
+                <div class="flex-auto flex flex-col">
+                    <label for="desk-popks">Deskripsi</label>
+                    <textarea id="desk-popks" type="text" class="rounded-md h-[70px] text-black hide-scrollbar resize-none"></textarea>
+                </div>
+
+                <div class="mt-4 flex justify-end">
+                    <button type="submit" name="Send" class=" w-full  md:w-[188px] bg-secondary text-white text-sm text-center h-[37px] rounded-md hover:scale-95 duration-200">Send</button>
                 </div>
             </div>
-        </div>
+
+            <div class="flex justify-between items-center pt-4 md:mb-0">
+                <div>
+                    <a href="/client/order/plan/percobaan">
+                        <div class="bg-secondary text-white text-sm text-center py-1 px-3 md:px-14 rounded-md font-bold hover:scale-95 duration-200">
+                            <p class=" hidden md:inline">Back</p>        
+                            <i class="ri-arrow-left-line inline md:hidden"></i>  
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="flex gap-4 max-sm:w-full max-sm:justify-between">
+                    <div></div>
+                    <div>
+                        <button type="submit" name="save" class=" w-full bg-secondary text-white text-sm text-center py-1 px-14 rounded-md font-bold hover:scale-95 duration-200">Save</button>
+                    </div>
+        
+                    <div>
+                        <a href="/client/order"></a>
+                        <div class=" bg-secondary text-white text-sm text-center py-1 px-3 md:px-14 rounded-md font-bold hover:scale-95 duration-200">
+                            <p class="hidden md:inline">Finish</p> 
+                            <i class="ri-check-line block md:hidden"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 @endsection
