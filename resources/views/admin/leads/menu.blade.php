30ee2784f0da7a6f48ec6b854c00eac43c1fc390
kemalzaki <kemalmuhammadzaki@gmail.com>
2023-08-23 11:11:19 +0700

    minor fix menu leads

--- resources/views/admin/leads/menu.blade.php
+++ resources/views/admin/leads/menu.blade.php
@@ -107,7 +107,7 @@
                 </tbody>
             </table>
         </div>
-        <div class="sticky bottom-0 flex justify-center items-center gap-3">
+        <div class="sticky bottom-0 bg-darksecondary flex justify-center items-center gap-3">
             {{ $leads -> links('vendor.pagination.custom-pagination') }}
         </div>
     </div>
