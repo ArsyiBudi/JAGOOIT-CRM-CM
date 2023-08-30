4bc1f0ad8031e461cd1294ad86e5378c430e19d8
kemalzaki <kemalmuhammadzaki@gmail.com>
2023-08-30 11:31:55 +0700

    fixed header table client menu

--- resources/views/admin/client/menu.blade.php
+++ resources/views/admin/client/menu.blade.php
@@ -46,7 +46,7 @@
 </div>
 <div class="overflow-auto bg-darkSecondary lg:pt-0 h-[90vh] rounded-md">
     <div class="w-full px-8 flex flex-col">
-        <div class="sticky top-0 pt-10 bg-darkSecondary">
+        <div class="pt-10 bg-darkSecondary">
             <div class="border-b border-white w-full pb-3 mb-3">
                 <h3 class="text-white font-semibold text-3xl">Data Clients</h3>
             </div>
@@ -73,9 +73,9 @@
             <div class=" border-b border-white w-full rounded-lg mt-4"></div>
         </div>
 
-        <div class=" hide-scrollbar w-full mt-5 h-full overflow-auto pr-2">
+        <div class=" hide-scrollbar w-full mt-5 max-h-96 overflow-auto pr-2">
             <table class=" w-full text-xs md:text-sm font-bold ">
-                <thead>
+                <thead class="bg-darkSecondary sticky top-0">
                     <tr>
                         <td align="center" class="pb-4">No</td>
                         <td align="center" class="pb-4">Nama Perusahaan</td>
