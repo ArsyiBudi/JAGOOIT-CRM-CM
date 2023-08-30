8408d773c935fe1b47433a868216053cfdd61d7e
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2023-08-30 08:59:02 +0700

    penawaran input tags

--- app/Http/Controllers/PenawaranWordController.php
+++ app/Http/Controllers/PenawaranWordController.php
@@ -14,21 +14,27 @@ class PenawaranWordController extends Controller
 
         $phpWord = new \PhpOffice\PhpWord\TemplateProcessor('template.docx');
 
-        $phpWord->setValue('perihal', $input['perihal']);
-        $phpWord->setValue('kepada', $input['kepada']);
-        $phpWord->setValue('tempat', $input['tempat']);
-        $phpWord->setValue('tanggal', $input['tanggal']);
-        $phpWord->setValue('ditawarkan', $input['ditawarkan']);
-        $phpWord->setValue('jumlahTalent', $input['jumlahTalent']);
-        $phpWord->setValue('weekday', $input['weekday']);
-        $phpWord->setValue('weekend', $input['weekend']);
-        $phpWord->setValue('konsumsi', $input['konsumsi']);
-        $phpWord->setValue('transPP', $input['transPP']);
+        $phpWord->setValue('perihal', $input['offer_subject']);
+        $phpWord->setValue('kepada', $input['recipient_name']);
+        $phpWord->setValue('tempat', $input['location']);
+        $phpWord->setValue('tanggal', $input['date']);
+        $phpWord->setValue('ditawarkan', $input['context']);
+        $phpWord->setValue('jumlahTalent', $input['talent_total']);
+        $phpWord->setValue('weekday', $input['weekday_cost']);
+        $phpWord->setValue('weekend', $input['weekend_cost']);
+        $phpWord->setValue('konsumsi', $input['consumtion_cost']);
+        $phpWord->setValue('transPP', $input['transportation_cost']);
 
+        $replc = array(
+                array('qty'=> '12', 'needed_job'=> 'be', 'city_location' => 'Liverpool'),
+                array('qty'=> '11', 'needed_job'=> 'fe', 'city_location' => 'Manchester'),
+                array('qty'=> '1', 'needed_job'=> 'be', 'city_location' => 'Londo'),
+            );
 
+        $phpWord->cloneBlock('table_block_placeholder', 0, true, false, $replc);
         $tempFilePath = tempnam(sys_get_temp_dir(), 'Surat_Penawaran');
         $phpWord->saveAs($tempFilePath);
-
+        
 
         $headers = [
             'Content-Type' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
